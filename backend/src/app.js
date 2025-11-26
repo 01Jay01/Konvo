a8767ee2d1c9f1cb13c92d7342e691ed18017fdf
Jay <jaychaurasia751@gmail.com>
2025-11-26 18:04:14 +0530

    Removed frontend folder from Git

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import connectToSocket from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+
+const server = createServer(app);
+const io = connectToSocket(server)
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async() => {
+    const connectionDb = await mongoose.connect("mongodb+srv://jaychaurasia751:pass&WORD0@cluster0.ymxhp4w.mongodb.net/")
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("Listen on port 8000");
+        
+    })
+}
+
+start();
